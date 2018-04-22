f760bd2f4355abebb4574690dfbc740d14b5028e
jeevan-jp <jiwanprakash65@gmail.com>
2018-04-22 13:09:19 +0530

    Bug fixes

--- server.js
+++ server.js
@@ -98,27 +98,27 @@ var complaints = [
         status: 'close'
     },
     {
-        id: 15, title: 'Title', department: 'THIS IS BODY', 
+        id: 15, title: 'Title 1', department: 'THIS IS BODY', 
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
         status: 'open'
     },
     {
-        id: 16, title: 'Title', department: 'Electricity', 
+        id: 16, title: 'Title 2', department: 'Electricity', 
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
         status: 'invalid'
     },
     {
-        id: 17, title: 'Title', department: 'THIS IS BODY', 
+        id: 17, title: 'Title 3', department: 'THIS IS BODY', 
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
         status: 'assigned'
     },
     {
-        id: 18, title: 'Title', department: 'THIS IS BODY', 
+        id: 18, title: 'Title 4', department: 'THIS IS BODY', 
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
         status: 'open'
     },
     {
-        id: 19, title: 'Title', department: 'Electricity', 
+        id: 19, title: 'Title 5', department: 'Electricity', 
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
         status: 'close'
     },
